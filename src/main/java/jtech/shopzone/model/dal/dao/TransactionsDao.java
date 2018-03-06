566dee2bc74289311846b2fbcb25b20954ea6033
DESKTOP-AP2CQ3B\Mohamed Mahrou <mmrwwe@gmail.com>
2018-03-06 23:33:12 +0200

    fixed return type of getUserHistory.

--- src/main/java/jtech/shopzone/model/dal/dao/TransactionsDao.java
+++ src/main/java/jtech/shopzone/model/dal/dao/TransactionsDao.java
@@ -1,11 +1,12 @@
-package jtech.shopzone.controller;
+package jtech.shopzone.model.dal.dao;
 
 import jtech.shopzone.model.entity.UserProductsEntity;
 
 import java.util.ArrayList;
 
-public interface TransactionsController {
+public interface TransactionsDao {
 
     ArrayList<UserProductsEntity> getUserHistory(int userId);
+
 
 }
