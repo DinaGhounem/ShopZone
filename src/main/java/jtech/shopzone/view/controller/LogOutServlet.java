bc206844ddd922836c581f81376335bb6e602516
DESKTOP-AP2CQ3B\Mohamed Mahrou <mmrwwe@gmail.com>
2018-03-08 18:50:09 +0200

    logout servlet and session checker filter v1

---
+++ src/main/java/jtech/shopzone/view/controller/LogOutServlet.java
@@ -0,0 +1,28 @@
+package jtech.shopzone.view.controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+/**
+ * @author Muhammed Mahrous
+ */
+@WebServlet(name = "LogOutServlet")
+public class LogOutServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession httpSession = request.getSession();
+        if(httpSession!= null)
+        {
+            httpSession.invalidate();
+            response.sendRedirect("/signin.html");
+        }
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
